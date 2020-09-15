4e8b11bf81faf7a0b0b8f30bf33af0fa93088319
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-09-15 22:18:50 +0700

    fix rest concurrent

--- REST API/concurrent.js
+++ REST API/concurrent.js
@@ -1,61 +1,41 @@
-import Client from './client.js';
-import { performance } from 'perf_hooks';
-
-
+import Client from "./client.js";
+import { performance } from "perf_hooks";
 
 async function c1(t1) {
+  let client = new Client();
 
-    let client = new Client();
-    // call
-
-
-    // call
-    // for(let i=0; i<n; i++) {
-    //     let client = new Client();
-    let book = {
-        "id": 0,
-        "title": "Book C1" ,
-        "author": "Melvin",
-    };
-    await client.insertBook(book);
-    //     await client.getBookById(i);
-    //     await client.deleteBook(i);
-    // }
-    check(t1);
+  let n = Math.floor(Math.random() * 10) % 3;
+  let book = {
+    id: 0,
+    title: "Book C1",
+    author: "Melvin",
+  };
+  switch (n) {
+    case 0:
+      await client.insertBook(book);
+      break;
+    case 1:
+      await client.getBookById(0);
+      break;
+    case 2:
+      await client.deleteBook(0);
+      break;
+  }
+  check(t1);
 }
 
-// async function c2(n, t1) {
-//     for(let i=0; i<n; i++) {
-//         // console.log(i);
-//         let client = new Client();
-//         let book = {
-//             "id": i,
-//             "title": "Book C2" + i.toString(),
-//             "author": "Melvin",
-//         };
-//         await client.insertBook(book);
-//         await client.getBookById(i);
-//         await client.deleteBook(i);
-//     }
-//     check(t1);
-// }
 
 function check(t1) {
-    const t2 = performance.now();
-    console.log("use time = " + (t2 - t1).toString() + " ms."); 
-    
+  const t2 = performance.now();
+  console.log("use time = " + (t2 - t1).toString() + " ms.");
 }
 
 async function start() {
-    let t1 = performance.now();
-    let n = process.argv[2] | 1;
-    for(let i=0; i<n; i++){
-        c1(t1);
-    }
-    // c2(n,t1);
-    // check(t1);
-    // let t2 = performance.now();
-    // console.log("use time = " + (t2 - t1).toString() + " ms."); 
+  let t1 = performance.now();
+  let n = process.argv[2] | 1;
+  for (let i = 0; i < n; i++) {
+    c1(t1);
+  }
 }
 
 start();
